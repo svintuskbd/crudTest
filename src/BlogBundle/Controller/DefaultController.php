873e5a5fa15c0581bbd3e213eac23e2220d79e9b
svintuskbd <swintuskbd@gmail.com>
2018-06-08 21:39:59 +0300

    new template

--- src/BlogBundle/Controller/DefaultController.php
+++ src/BlogBundle/Controller/DefaultController.php
@@ -2,16 +2,23 @@
 
 namespace BlogBundle\Controller;
 
+use AppBundle\Entity\Article;
+use AppBundle\Entity\Category;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
 class DefaultController extends Controller
 {
-//    /**
-//     * @Route("/")
-//     */
-//    public function indexAction()
-//    {
-//        return $this->render('BlogBundle:Default:index.html.twig');
-//    }
+    /**
+     * @Route("/blog-list", name="blog_index_route")
+     */
+    public function indexAction()
+    {
+        $articleRepos = $this->getDoctrine()->getRepository(Article::class);
+        $articles = $articleRepos->findAll();
+
+        return $this->render('@Blog/CRUD/article_list.html.twig', [
+            'articles' => $articles
+        ]);
+    }
 }
